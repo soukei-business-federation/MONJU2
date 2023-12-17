65bb66a82e2f49bd62197097651d2b0bac512549
bookamong <haru20043750@gmail.com>
2023-12-17 18:43:53 +0900

    yattane

---
+++ Javascript/about.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    var scrollAnimationElm = document.querySelectorAll("#about_outline .about_MONJU2")
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 150;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('on');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+  };
